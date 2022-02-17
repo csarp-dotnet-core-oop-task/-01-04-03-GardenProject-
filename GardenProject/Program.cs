d21b0fb5ecfd68dc2d37e390de7536a7dda2ea7a
Gyuris Csaba <gycsaba72@gmail.com>
2022-02-17 10:13:03 +0100

    Update Program.cs

--- GardenProject/Program.cs
+++ GardenProject/Program.cs
@@ -1,18 +1,11 @@
 ﻿using System;
 
 namespace GardenProject
-{
-   
+{   
     class Program
-<<<<<<< HEAD
     {
         static void Main(string[] args)
         {
-
-
         }
-=======
-    {   
->>>>>>> b798b9a2ec258c4d9489270305b28ec318017745
     }
 }
